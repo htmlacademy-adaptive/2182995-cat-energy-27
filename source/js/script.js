59917dab39e0cc240b92aaf4abd355c1689d479e
Nastya <a.kochkina92@gmail.com>
2023-01-18 18:15:01 +0300

    бургерное меню

--- source/js/script.js
+++ source/js/script.js
@@ -1,14 +1,14 @@
-let navMain = document.querySelector('.main-nav');
+let navMain = document.querySelector('.main-nav__list');
 let navToggle = document.querySelector('.main-nav__toggle');
 
 navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav--closed')) {
-    navMain.classList.remove('main-nav--closed');
-    navMain.classList.add('main-nav--opened');
+  if (navMain.classList.contains('main-nav__list--closed')) {
+    navMain.classList.remove('main-nav__list--closed');
+    navMain.classList.add('main-nav__list--opened');
   } else {
-    navMain.classList.add('main-nav--closed');
-    navMain.classList.remove('main-nav--opened');
+    navMain.classList.add('main-nav__list--closed');
+    navMain.classList.remove('main-nav__list--opened');
   }
 });
